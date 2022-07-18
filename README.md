fbd733641ba743e9d5f9850900d9413b56761f61
jonironman <62795102+jonironman@users.noreply.github.com>
2022-07-18 20:22:25 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
+install 
+1. Node.Js
+2. install python
+3. install MongoDB compass
+
+
 # FastAPI-React-js-mongoDB
 https://www.youtube.com/watch?v=oGwszbCH5Z4
+
+
+# backend
+1. python - m venv virt
+2. activate virt
+3. pip install -r requirements.txt
+
+unicorn main:app --reload
+
+
+# frontend 
+npx create-react-app frontend
+
+# test npx
+frontend/ npx start
+# install lib
+npx install axios bootstrap
+
+
+## source https://www.youtube.com/watch?v=oGwszbCH5Z4
+## source github https://github.com/BekBrace/FARM-Stack-Course/tree/25e973366057420d1cb40e9d6cbf221e09b15165
---
+++ frontend/src/components/Todo.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React from "react";
+
+
+function TodoItem(props){
+    const deleteTodohandler = (title) => {
+        axios.delete(`http://localhost:8000/api/todo/${title}`)
+        .then (res => console.log(res.data))
+    }
+    return (
+        <div>
+            <p>
+                <span style={{ fontWeight: 'bold,underline'}}>{props.todo.title}</span>{props.todo.description}
+                <button onClick={ () => deleteTodohandler(props.todo.title) }
+                className="btn btn-outline-danger my-2 mx2"
+                style={{'borderRadius': '50px',}}>X</button>
+                <hr></hr>
+            </p>
+        </div>
+
+    )
+}
+
+
+export default TodoItem;
